0aca02a8892d23b0da62a132c9ac39c47c0a8fe8
gpffh20 <eunbin584836@gmail.com>
2023-06-22 16:51:10 +0900

    to form

--- elemental/user/forms.py
+++ elemental/user/forms.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.forms import TextInput
+from django import forms
 from .models import User
 
 class UserLoginForm(AuthenticationForm):
-    pass
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control'}),
+        label='유저이름'
+    )
 
 class UserSignUpForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
@@ -14,6 +18,14 @@ class UserSignUpForm(UserCreationForm):
             'username'
         ]
 
-        widgets = [
-            'username'
-        ]
+        widgets = {
+            'username' : TextInput(attrs={
+                'class': "form-control",
+                'style': "background-color: skyblue",
+                'placeholder': 'ID'
+            }),
+        }
+
+        labels = {
+            'username': 'ID',
+        }
